7c2a46250b08228c8fef9fb3a9b82f09847fa094
naveen <naveen@ascentic.se>
2022-10-17 22:23:10 +0530

    user > movies migrations added

---
+++ migrations/20221017163816_create_table_user_movie_watch_later.js
@@ -0,0 +1,19 @@
+const tableKey = 'user_movie_watch_later';
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async (knex) => knex.schema.createTable(tableKey, (table) => {
+  table.increments('id').primary().unsigned();
+  table.integer('user').notNullable().references('id').inTable('user');
+  table.string('movie_id').unique().notNullable();
+  table.timestamp('created_at').defaultTo(knex.fn.now());
+  table.timestamp('updated_at').defaultTo(knex.fn.now());
+});
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable(tableKey);
