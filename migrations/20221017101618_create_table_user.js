b605d5b322c5b2ec3890caceb8fc4c67849fe80b
naveen <naveen@ascentic.se>
2022-10-17 22:23:10 +0530

    user > movies migrations added

--- migrations/20221017101618_create_table_user.js
+++ migrations/20221017101618_create_table_user.js
@@ -10,6 +10,8 @@ exports.up = async (knex) => {
     table.string('email').unique().notNullable();
     table.string('first_name').notNullable();
     table.string('last_name').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 
   await knex(tableKey).insert({
